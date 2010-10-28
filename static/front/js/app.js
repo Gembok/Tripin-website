afb2511a4ed64b501ae37feb7f08c900c8c918fe
Damien Firmenich <fir.damien@gmail.com>
2010-10-28 21:19:50 +0200

    hashlisten working now (changed hash regex) added media page design

--- static/front/js/app.js
+++ static/front/js/app.js
@@ -1,3 +1,14 @@
+$(function() {
+	$('#media-photos').live('mouseenter', function() { $('span#photos').fadeIn(200);});
+	$('#media-photos').live('mouseleave', function() { $('span#photos').fadeOut(300);});
+
+	$('#media-videos').live('mouseenter', function() { $('span#videos').fadeIn(200);});
+	$('#media-videos').live('mouseleave', function() { $('span#videos').fadeOut(300);});
+
+	$('#media-presse').live('mouseenter', function() { $('span#presse').fadeIn(200);});
+	$('#media-presse').live('mouseleave', function() { $('span#presse').fadeOut(300);});
+});
+
 $.hashListen('/', function() {
 	slideUp();
 });
@@ -9,30 +20,35 @@ $.hashListen('/:action/?', function(action) {
 	});
 });
 
-$.hashListen('/:action/:id/?', function(action, id) {
+$.hashListen('/:action/([0-9]+)/?', function(action, id) {
+	var top = 500;
+	if (action == 'media') {
+		top = 300;
+	}
 	var url = '/'+action+'/'+id;
-	console.log(url);
 	$.getJSON(url, null, function(data) {
-		fill(data);
+		fill(data, top);
 	});
 });
 
 
-function slideDown(fn) {
-	$('#container').animate({top: '500px'}, 300, 'swing', fn);
+function slideDown(top) {
+	var top = 500;
+	$('#slide').height(top);
+	$('#container').animate({top: top}, 300, 'swing');
 }
 
 function slideUp() {
 	$('#page').fadeOut(150);
-	$('#container').animate({top: '0px'}, 300, 'swing');
+	$('#container').animate({top: 0}, 300, 'swing');
 }
 
-function fill(data) {
+function fill(data, top) {
 	var html = Mustache.to_html(data.template, data.data);
 	
 	$('#page').fadeOut(150, function() {
 		$(this).html(html);
-		slideDown();
+		slideDown(top);
 		$(this).fadeIn(500);
 	});
 }
